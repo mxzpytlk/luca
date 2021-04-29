3d714dbd858bf95934687320b2f0dbd86ca394ae
mxzpytlk <mxzpytlk@gmail.com>
2021-04-29 23:48:25 +0300

    Add auth component

---
+++ src/views/auth/Auth.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div class="auth">
+        <span class="auth__logo">
+          LUCA
+        </span>
+        <form class="auth__form">
+          <input type="text" class="auth__input luca-input" name="login" placeholder="Login">
+          <input type="password" class="auth__input luca-input" name="pass" placeholder="Password">
+          <input type="submit" class="auth__submit" value="entry">
+        </form>
+      </div>
+  </div>
+</template>
+
+<script>
+import './auth.scss';
+
+export default {};
+</script>
