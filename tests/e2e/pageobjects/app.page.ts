ae93332170ab59e6e57453a380fb0701f097ce4e
mxzpytlk <mxzpytlk@gmail.com>
2021-04-29 22:02:48 +0300

    init

---
+++ tests/e2e/pageobjects/app.page.ts
@@ -0,0 +1,15 @@
+class App {
+  /**
+   * elements
+   */
+  get heading() { return $('h1'); }
+
+  /**
+   * methods
+   */
+  public open(path = '/') {
+    browser.url(path);
+  }
+}
+
+export default new App();
