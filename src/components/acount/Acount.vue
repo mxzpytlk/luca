7faabe1e8b5b83d81bff5b2cde1aac6ea7728b65
mxzpytlk <mxzpytlk@gmail.com>
2021-05-05 20:05:44 +0300

    Add user name to acount sidebar

--- src/components/acount/Acount.vue
+++ src/components/acount/Acount.vue
@@ -1,26 +1,31 @@
 <template>
   <div class="acount" v-bind:class="{ 'acount__active' : isAcount }">
     <font-awesome-icon
-      icon="arrow-alt-circle-right"
-      size="3x"
-      class="acount__icon_right"
-      @click.prevent="close"
-    />
-    <font-awesome-icon
-      icon="sign-out-alt"
-      size="3x"
-      class="acount__icon_out"
-      @click.prevent="logout"
-    />
-  </div>
+        icon="arrow-alt-circle-right"
+        size="3x"
+        class="acount__icon_right"
+        @click.prevent="close"
+      />
+      <div class="acount__content">
+        <h2 class="acount__name">{{name}}</h2>
+      </div>
+      <font-awesome-icon
+        icon="sign-out-alt"
+        size="3x"
+        class="acount__icon_out"
+        @click.prevent="logout"
+      />
+    </div>
 </template>
 
 <script>
 import { RouterPath } from '@/core/enums/router-path';
 import './acount.scss';
+import { mapGetters } from 'vuex';
 
 export default {
   computed: {
+    ...mapGetters(['name']),
     isAcount() {
       return this.$route.path === `${RouterPath.MAIN}/${RouterPath.ACOUNT}`;
     },
--- src/store/modules/auth.ts
+++ src/store/modules/auth.ts
@@ -22,17 +22,20 @@ export default {
     logout(state: any) {
       state.status = '';
       state.token = '';
+      state.name = '';
     },
+
   },
+
   getters: {
     isLoggedIn: (state: any) => !!state.token,
     authStatus: (state: any) => state.status,
     name: (state: any) => state.name,
   },
+
   actions: {
 
     async login({ commit }: any, loginData: IAuthData) {
-    // TODO Make login by connection with server
       const { name, pass } = loginData;
       if (!name || !pass) {
         throw new Error('Name and password can\'t be empty');
@@ -42,7 +45,7 @@ export default {
       const token = res?.data?.token;
       if (token) {
         setInLocalStorage(LocalStorageKey.TOKEN, token);
-        setInLocalStorage(LocalStorageKey.NAME, name);
+        localStorage.setItem(LocalStorageKey.NAME, name);
         commit('auth_success', { token, name });
       } else {
         throw new Error('Login or password is incorrect');
@@ -61,6 +64,7 @@ export default {
 
     async logout({ commit }: any) {
       localStorage.removeItem(LocalStorageKey.TOKEN);
+      localStorage.removeItem(LocalStorageKey.NAME);
       commit('logout');
     },
   },
