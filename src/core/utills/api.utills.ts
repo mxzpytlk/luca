3b09aa98c4300050bdd1da61b3d7416b6f97d899
mxzpytlk <mxzpytlk@gmail.com>
2021-05-06 00:36:31 +0300

    Refactor api

--- src/core/utills/api.utills.ts
+++ src/core/utills/api.utills.ts
@@ -1,29 +1,29 @@
 import axios from 'axios';
 import config from '../../assets/config.json';
 import { ApiHref } from '../enums/api.enum';
+import auth from '../../store/modules/auth';
 
 export function getUrlByHref(href: ApiHref): string {
   return config.url + href;
 }
 
 export async function post(href: ApiHref, data?: any): Promise<any> {
-  try {
-    return await axios({
-      method: 'POST',
-      url: getUrlByHref(href),
-      params: data,
-    });
-  } catch (e) {
-    throw e?.response?.data;
-  }
+  return await makeRequest(href, 'POST', data);
 }
 
 export async function get(href: ApiHref, data?: any): Promise<any> {
+  return await makeRequest(href, 'GET', data);
+}
+
+async function makeRequest(href: ApiHref, method: any, data?: any): Promise<any> {
   try {
     return await axios({
-      method: 'GET',
+      method,
       url: getUrlByHref(href),
-      params: data,
+      params: {
+        ...data,
+        userId: auth.getters.userId,
+      },
     });
   } catch (e) {
     throw e?.response?.data;
